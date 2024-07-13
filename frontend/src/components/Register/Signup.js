63cee8b58eac0e2dd7dd425cc8b3dd5477793691
pavnsai <pavansai18.a@gmail.com>
2024-07-12 23:49:11 -0500

    Fixed minor issues

--- frontend/src/components/Register/Signup.js
+++ frontend/src/components/Register/Signup.js
@@ -16,7 +16,7 @@ import './Signup.css';
 const SignupSchema = Yup.object().shape({
     firstName: Yup.string().required('First Name is required'),
     lastName: Yup.string().required('Last Name is required'),
-    password: Yup.string().required('Password is required'),
+    password: Yup.string().min(6, 'Password must be at least 6 characters').required('Password is required'),
     confirmPassword: Yup.string()
         .oneOf([Yup.ref('password'), null], 'Passwords must match')
         .required('Confirm Password is required'),
